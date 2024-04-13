d255ce8401c6b25671f0ef28220a025e5329fee8
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2024-04-13 20:30:42 +0530

    Added NewIssue Editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,18 @@
-'use client';
+"use client";
 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+import { Button, TextField } from "@radix-ui/themes";
+import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
-    <div className=' max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title'></TextField.Root>
-      <TextArea placeholder='Description' />
+    <div className=" max-w-xl space-y-3">
+      <TextField.Root placeholder="Title"></TextField.Root>
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
