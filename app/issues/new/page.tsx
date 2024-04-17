193ef256acd35e972607bcc5c77ec29f02a065c2
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2024-04-17 19:43:13 +0530

    Added Client-side Validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,15 +7,17 @@ import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import z from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
   return (
     <div className=" max-w-xl">
@@ -37,6 +39,7 @@ const NewIssuePage = () => {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
+        {errors.title && <p className="text-red-500">{errors.title.message}</p>}
         <Controller
           name="description"
           control={control}
@@ -44,6 +47,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && <p className="text-red-500">{errors.description.message}</p>}
         <Button>Submit New Issue</Button>
       </form>
     </div>
